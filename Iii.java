405799587156c4a1482b39bfb5b11fdec6c329f5
604461194 <42225671+604461194@users.noreply.github.com>
2018-08-09 11:38:59 +0800

    滴答声大

---
+++ Iii.java
@@ -0,0 +1,14 @@
+   public class Iii{
+      public static void main(String[]args){
+                    
+    System.out.println("Äã´óÒ¯\tÄã´ó½ã\n");
+	System.out.println("Äã´óÒ¯\tÄã´ó½ã");
+  
+      }
+
+
+
+
+
+
+   }
---
+++ QDD.java
@@ -0,0 +1,7 @@
+   public slass QDD{
+       public static void main(String[]args){
+       System.out.println("java");
+       
+       }
+
+   }
